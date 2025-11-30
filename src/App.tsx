5e3098eea314309061185a335e7c81f735b2c84d
HenryArin <128102839+HenryArin@users.noreply.github.com>
2025-11-30 03:03:29 -0800

    added the blog section with skelton css

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import HeroSection from './sections/HeroSection';
 import AboutSection from './sections/AboutSection';
 import ProjectsSection from './sections/ProjectsSection';
+import BlogSection from './sections/BlogSection';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <HeroSection />
       <AboutSection />
       <ProjectsSection />
+      <BlogSection />
     </div>
   );
 }
