31c3130a5cf97d8e134fd8a5590b00c8efece29d
HenryArin <128102839+HenryArin@users.noreply.github.com>
2025-11-30 02:39:49 -0800

    made the skeleton for the HeroSection and had it rendered in app.tsx

---
+++ src/sections/HeroSection.tsx
@@ -0,0 +1,29 @@
+const HeroSection = () => {
+    return (
+      <section>
+  
+        <div>
+          <img src="/placeholder-profile.png" alt="Profile" />
+        </div>
+  
+        <h1>Henry Arinaga</h1>
+        <div className="name-divider"></div>
+        <p>Shoot for the stars, one commit at a time</p>
+  
+        <div>
+          <span>[GitHub]</span>
+          <span>[LinkedIn]</span>
+          <span>[Email]</span>
+          <span>[Discord]</span>
+        </div>
+  
+        <div>
+          <span>↓ Scroll</span>
+        </div>
+  
+      </section>
+    );
+  };
+  
+  export default HeroSection;
+  
